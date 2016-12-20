fca9ddcda8a7396a850501d74cacb9010f79f185
= <=>
2016-12-20 12:26:01 -0200

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: 'messages#message_of_the_day'
   get '/mensagem-do-dia', to: 'messages#message_of_the_day'
 end
